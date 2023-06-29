458cd12a5be19dac538d0d7f6604251e4159bd0c
unknown <moses.karanja@strathmore.edu>
2023-06-29 14:26:44 +0300

    user table created

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+
+    //Read all users
+    public function read()
+    {
+        $users = User::all();
+
+        if (count($users) > 0) {
+            return response()->json(["status" => "success", "message" => "Users attached in user object", "users" => $users]);
+        } else {
+            return response()->json(["status" => "success", "message" => "No users saved yet", "users" => $users]);
+        }
+    }
+
+    //create read update delete
+    public function create(Request $request)
+    {
+        $request->validate([
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'phone_number' => 'required|string',
+            'password' => 'required|confirmed',
+        ]);
+
+
+        $user = new User();
+
+        $user->first_name = $request['first_name'];
+        $user->last_name = $request['last_name'];
+        $user->phone_number = $request['phone_number'];
+        $user->password = $request['password'];
+
+        if ($user->save()) {
+            return response()->json(["status" => "success", "message" => "User Created"]);
+        } else {
+            return response()->json(["status" => "error", "message" => "User Failed to Create"]);
+        }
+    }
+
+
+    //update a user
+    public function updateUser(Request $request)
+    {
+       $request->validate([
+            'user_id' => 'required',
+            'first_name' => 'nullable',
+            'last_name' => 'nullable',
+            'phone_number' => 'required|string',
+
+        ]);
+
+        $user = User::where('id', $request->user_id)->first();
+
+        if($user){
+            $user = new User();
+            $user->first_name = $request['first_name'];
+            $user->last_name = $request['last_name'];
+            $user->phone_number = $request['phone_number'];
+
+            if($user->save()){
+                return response()->json(["status" => "success", "message" => "User Updated Successfully", "user" => $user]);
+
+            }
+            else{
+                return response()->json(["status" => "error", "message" => "User Failed to Update"]);
+
+            }
+
+        }
+        else{
+            return response()->json(["status" => "success", "message" => "No User with id ". $request->user_id. " found"]);
+
+        }
+
+    }
+
+    public function deleteUser(Request $request)
+    {
+       $request->validate([
+            'user_id' => 'required',
+
+        ]);
+
+        $user = User::where('id', $request->user_id)->first();
+
+        if($user){
+
+            if($user->delete()){
+                return response()->json(["status" => "success", "message" => "User Deleted Successfully", "user" => $user]);
+
+            }
+            else{
+                return response()->json(["status" => "error", "message" => "User Failed to Delete"]);
+
+            }
+
+        }
+        else{
+            return response()->json(["status" => "success", "message" => "No User with id ". $request->user_id. " found"]);
+
+        }
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/readUsers', [UserController::class, 'read']);
+Route::post('/createUser', [UserController::class, 'create']);
+Route::post('/updateUser', [UserController::class, 'updateUser']);
+Route::post('/deleteUser', [UserController::class, 'deleteUser']);
+
